ba9a3ed20d03fe45f61ab085aa07f0a262de4f11
Ricardo Costa <76919550+R1c4rdCo5t4@users.noreply.github.com>
2022-12-02 21:57:34 +0000

    Handled KeyboardInterrupt

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ from usage import Usage
 
 def main():
 
-    while True:
-        cpu_usage = Usage(psutil.cpu_percent())
-        ram_usage = Usage(psutil.virtual_memory().percent)
-        disk_usage = Usage(psutil.disk_usage('/').percent)
-        
-        print(f"\rCPU Usage: {str(cpu_usage)}", end="")
-        print(f" RAM Usage: {str(ram_usage)}", end="")
-        print(f" Disk Usage: {str(disk_usage)}", end="\r")
-        time.sleep(0.5)
+    try:
+        while True:
+            cpu_usage = Usage(psutil.cpu_percent())
+            ram_usage = Usage(psutil.virtual_memory().percent)
+            disk_usage = Usage(psutil.disk_usage('/').percent)
+            
+            print(f"\rCPU Usage: {str(cpu_usage)}", end="")
+            print(f" RAM Usage: {str(ram_usage)}", end="")
+            print(f" Disk Usage: {str(disk_usage)}", end="\r")
+            time.sleep(0.5)
 
+    except KeyboardInterrupt:
+        clear_cmd()
+        os._exit(0)
+
+
+def clear_cmd() -> None:
+    os.system('clear' if os.name == 'posix' else 'cls') # linux/windows
 
 if __name__ == '__main__':
-    os.system('clear' if os.name == 'posix' else 'cls') # linux/windows
+    clear_cmd()
     main()
